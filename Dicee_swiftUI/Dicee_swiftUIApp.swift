f0a039018859db7e0cbbc45c255e4b32c2d1c33f
Shueib Abdirahman <136870666+shueibka@users.noreply.github.com>
2024-01-30 23:16:05 +0100

    Initial Commit

---
+++ Dicee_swiftUI/Dicee_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_swiftUIApp.swift
+//  Dicee_swiftUI
+//
+//  Created by Fahad nur on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
